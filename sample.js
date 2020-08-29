610909f2030c0a8f21cc323a8d09fda3bf8f3bda
yomono <31445936+yomono@users.noreply.github.com>
2020-08-29 02:33:15 -0300

    Update sample.js

--- sample.js
+++ sample.js
@@ -12,15 +12,15 @@ module.exports.sample = function (parent) {
     var obj = {};
     obj.parent = parent; // keep a reference to the parent
     obj.exports = [
-      "onDesktopDisconnect",
+     // "onDesktopDisconnect",
        "onWebUIStartupEnd"// export this function to the web UI
     ];
     
-    obj.onDesktopDisconnect = function() {  // this is called when the desktop disconnect button is clicked
-        writeDeviceEvent(encodeURIComponent(currentNode._id));  // mimic what the button does on the device main page to pull up a log
-        Q('d2devEvent').value = Date().toLocaleString()+': '; // pre-fill the date for a timestamp
-        focusTextBox('d2devEvent');
-    };
+  //  obj.onDesktopDisconnect = function() {  // this is called when the desktop disconnect button is clicked
+  //      writeDeviceEvent(encodeURIComponent(currentNode._id));  // mimic what the button does on the device main page to pull up a log
+  //      Q('d2devEvent').value = Date().toLocaleString()+': '; // pre-fill the date for a timestamp
+   //     focusTextBox('d2devEvent');
+  //  };
     
     obj.onWebUIStartupEnd = function() {
         alert('probando');
@@ -29,7 +29,7 @@ module.exports.sample = function (parent) {
         if (as) as.parentNode.removeChild(as);
         var x = '<span id="plugin_testcristianSettings" style="display: block;"><a onclick="pluginHandler.routeplus.openSettings();">aver</a></span>';
         ld.innerHTML += x;*/
-    };
+    }
     
     return obj;
 }
